15ab459ffdfdd47eefbe99033b4888fca0d0b734
Satya_Vineeth <satyavineeth123@gmail.com>
2025-06-04 11:39:17 +0530

    Create AttendanceApp.java

---
+++ src/main/java/com/example/attendance/AttendanceApp.java
@@ -0,0 +1,9 @@
+package com.example.attendance;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AttendanceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceApp.class, args);
+    }
+}
